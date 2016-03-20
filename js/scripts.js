11bcb111e6a2e819d3ca6be18c710fdc3005e10d
Priya Vasudevan <satipriya99@gmail.com>
2016-03-20 15:18:53 -0700

    rewrite the entire BDD logic from scratch.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,20 @@
 //business logic
-var pingPong = function(pingPong){
-  var num =[];
+function pingPong(inputNumber) {
+  var output = [];
 
-  for (var i=1; i<=pingPong; i++){
-    if((i % 3 === 0) && (i % 5 === 0)){
-      num.push("pingPong");
-    } else if (i % 3 === 0){
-      num.push("ping");
-    } else if (i % 5 === 0){
-      num.push("Pong");
+  for (var i = 1; i <= inputNumber; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      output.push("pingpong");
+    } else if (i % 3 === 0) {
+      output.push("ping");
+    } else if (i % 5 === 0) {
+      output.push("pong");
     } else {
-      num.push(i)
-    };
+      output.push(i);
+    }
+  }
+  return output ;
   };
-  return num;
-};
 
 
-$(document).ready(function(){
-
-  $("form#numbers").submit(function(event){
-    $("#results").empty();
-
-    var number = parseInt($("#input#number").val());
-    var result = pingPong(number);
-
-    result.forEach(function(num){
-        var li = $("#results").append("<li>" + num + "</li>");
-      });
-        $("#results").show();
-
-    event.preventDefault();
-  });
-});
+  //user logic
